97423a0a459065adc822f5aac91e3f485d506d39
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2023-07-26 15:09:47 +0700

    fix cors

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ import { Server } from 'socket.io';
 const fastify = Fastify();
 
 const corsParam = {
-  origin: 'https://rplacejs.vercel.app', // dev origin: 'http://localhost:5173',
-  methods: ['GET'],
+  origin: 'https://rplacejs.vercel.app/', // dev origin: 'http://localhost:5173',
+  methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
